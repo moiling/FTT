7edc9b508920e0b6cfca47077878962b4f925e56
moiling <super8moi@gmail.com>
2015-04-07 15:01:23 +0800

    sqlite
    add and clear... can't choose item delete

--- FreeTimetableTest/app/src/main/java/com/moi/freetimetabletest/MainActivity.java
+++ FreeTimetableTest/app/src/main/java/com/moi/freetimetabletest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.moi.freetimetabletest;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -12,7 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 import android.view.inputmethod.InputMethodManager;
@@ -31,7 +33,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+public class MainActivity extends ActionBarActivity implements View.OnClickListener, Toolbar.OnMenuItemClickListener, AdapterView.OnItemLongClickListener {
 
     private MaterialMenuDrawable materialMenu;
 
@@ -59,16 +61,40 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     private Animation animation;
 
-    private AnimationSet animationSet;
-
     private LayoutAnimationController controller;
 
     private RelativeLayout relativeLayout;
+
+    //数据库
+
+    private TableDatabaseHelper dbHelper;
+
+    private SQLiteDatabase db;
+
+    private ContentValues values;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 创建数据库
+        dbHelper = new TableDatabaseHelper(this, "TimeTableStore.db", null, 1);
+        dbHelper.getWritableDatabase();
+        db = dbHelper.getWritableDatabase();
+        values = new ContentValues();
+
+        // 绑定list
+        Cursor cursor = db.query("timeTable", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(cursor.getColumnIndex("table_name"));
+                Table table = new Table(name);
+
+                tableList.add(table);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
         // 设置Toolbar
         setToolbar();
         // 为了一口气关掉活动
@@ -85,37 +111,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         // 当listview中没有item的时候显示的背景提示
         textViewHint = (TextView) findViewById(R.id.tv_hint_add_table);
         // 判断提示是否出现（当list为空时）
-        if (tableList.isEmpty()) {
-            textViewHint.setVisibility(View.VISIBLE);
-        } else {
-            textViewHint.setVisibility(View.GONE);
-        }
+        showHint();
+
         // 创建listview
         mListView = (BounceListView) findViewById(R.id.list_view);
         adapter = new TableListAdapter(MainActivity.this, R.layout.item_table_list, tableList);
         mListView.setAdapter(adapter);
-        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-
-                Animation shake = AnimationUtils.loadAnimation(view.getContext(),R.anim.shake);
-                view.startAnimation(shake);
-                SnackBar snackbar = new SnackBar((Activity) view.getContext(), "Delete this item?", "Yes", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        tableList.remove(position - 1);
-                        if (tableList.isEmpty()) {
-                            textViewHint.setVisibility(View.VISIBLE);
-                        } else {
-                            textViewHint.setVisibility(View.GONE);
-                        }
-                        adapter.notifyDataSetChanged();
-                    }
-                });
-                snackbar.show();
-                return true;
-            }
-        });
+        mListView.setOnItemLongClickListener(this);
 
         createTableLayout = (LinearLayout) findViewById(R.id.ll_create);
         createTableLayout.setVisibility(View.GONE);
@@ -138,7 +140,19 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     }
 
     /**
+     * 判断是否显示提示……
+     */
+    private void showHint() {
+        if (tableList.isEmpty()) {
+            textViewHint.setVisibility(View.VISIBLE);
+        } else {
+            textViewHint.setVisibility(View.GONE);
+        }
+    }
+
+    /**
      * 设置Toolbar
+     * 内有点击清空table
      */
     private void setToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -178,13 +192,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                         snackbar = new SnackBar((Activity) v.getContext(), "Clear all items?", "Yes", new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                tableList.clear();
-                                if (tableList.isEmpty()) {
-                                    textViewHint.setVisibility(View.VISIBLE);
-                                } else {
-                                    textViewHint.setVisibility(View.GONE);
-                                }
-                                adapter.notifyDataSetChanged();
+                                // 清空table
+                                clearTables();
                                 materialMenu.animateIconState(MaterialMenuDrawable.IconState.BURGER, false);
                             }
                         });
@@ -194,13 +203,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                     case CHECK:
                         if (!tableList.isEmpty()) {
                             if (snackbar.isShowing()) {
-                                tableList.clear();
-                                if (tableList.isEmpty()) {
-                                    textViewHint.setVisibility(View.VISIBLE);
-                                } else {
-                                    textViewHint.setVisibility(View.GONE);
-                                }
-                                adapter.notifyDataSetChanged();
+                                // 清空table
+                                clearTables();
                                 snackbar.dismiss();
                             }
                         }
@@ -223,78 +227,108 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this, "Click menu", Toast.LENGTH_SHORT).show();
             }
         });*/
-        toolbar.setOnMenuItemClickListener(onMenuItemClickListener);
+        toolbar.setOnMenuItemClickListener(this);
     }
 
     /**
-     * Toolbar item的点击事件
+     * 清空所有table
      */
-    private Toolbar.OnMenuItemClickListener onMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
+    private void clearTables() {
+        tableList.clear();
+        // 清空数据库
+        db.execSQL("DELETE FROM timeTable");
+        showHint();
+        adapter.notifyDataSetChanged();
+    }
 
-        @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                //创建一个新的list item（这里还没做好，不能命名和保存到数据库）
-                case R.id.action_edit:
-                    if (createTableLayout.getVisibility() == View.GONE) {
-                        createTableLayout.setVisibility(View.VISIBLE);
-                        animation = new AlphaAnimation(0,1);
-                        animation.setDuration(500);
-                        //animationSet.addAnimation(animation);
-                        controller = new LayoutAnimationController(animation,1);
-                        relativeLayout.setLayoutAnimation(controller);
-                        //textViewHint.setVisibility(View.GONE);
-                    } else {
+    /**
+     * 新建一个table
+     */
+    private void createTable() {
+        tableName = editText.getText().toString();
+        //数据库
+        values.clear();
+        values.put("table_name", tableName);
+        db.insert("timeTable", null, values);
 
-                        tableName = editText.getText().toString();
-                        Table table = new Table(tableName);
-                        tableList.add(table);
-                        if (tableList.isEmpty()) {
-                            textViewHint.setVisibility(View.VISIBLE);
-                        } else {
-                            textViewHint.setVisibility(View.GONE);
-                        }
-                        adapter.notifyDataSetChanged();
-                        createTableLayout.setVisibility(View.GONE);
-                        editText.setText("");
-                        inputMethodManager = (InputMethodManager)getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                        inputMethodManager.hideSoftInputFromWindow(editText.getWindowToken(), 0);
-                        if (tableList.isEmpty()) {
-                            textViewHint.setVisibility(View.VISIBLE);
-                        } else {
-                            textViewHint.setVisibility(View.GONE);
-                        }
+        Table table = new Table(tableName);
+        tableList.add(table);
+        adapter.notifyDataSetChanged();
+        createTableLayout.setVisibility(View.GONE);
+        editText.setText("");
+        inputMethodManager = (InputMethodManager)getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+        showHint();
+    }
 
-                    }
-                   /* LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
-                    final View view = inflater.inflate(R.layout.editbox_layout, null);
-                    final EditText editText = (EditText) view.findViewById(R.id.ed_table_name);
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Create a table")
-                            .setView(view)
-                            .setPositiveButton("CREATE",
-                                    new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            tableName = editText.getText().toString();
-                                            Table table = new Table(tableName);
-                                            tableList.add(table);
-                                            if (tableList.isEmpty()) {
-                                                textViewHint.setVisibility(View.VISIBLE);
-                                            } else {
-                                                textViewHint.setVisibility(View.GONE);
-                                            }
-                                            adapter.notifyDataSetChanged();
-                                        }
-                                    }).create().show();*/
-                    break;
-                //case R.id.action_settings:
-                    //msg += "Click setting";
-                    //break;
-            }
-            return true;
+    /**
+     * 创建table
+     * 取消创建
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_create:
+
+                createTable();
+
+                break;
+            case R.id.bt_cancel:
+                createTableLayout.setVisibility(View.GONE);
+                showHint();
+                inputMethodManager = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                break;
+
         }
-    };
+
+    }
+
+    /**
+     *  item的长按事件（删除item）
+     */
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+        Animation shake = AnimationUtils.loadAnimation(view.getContext(),R.anim.shake);
+        view.startAnimation(shake);
+        SnackBar snackbar = new SnackBar((Activity) view.getContext(), "Delete this item?", "Yes", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tableList.remove(position - 1);// 这里-1是因为listView加了一个header，原来每个item的position都+1了，所以这里-1补平
+
+                showHint();
+                adapter.notifyDataSetChanged();
+            }
+        });
+        snackbar.show();
+        return true;
+    }
+
+    /**
+     * Toolbar item的点击事件（创建table）
+     */
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            //创建一个新的list item（这里还没做好，不能和保存到数据库）
+            case R.id.action_edit:
+                if (createTableLayout.getVisibility() == View.GONE) {
+                    createTableLayout.setVisibility(View.VISIBLE);
+                    animation = new AlphaAnimation(0,1);
+                    animation.setDuration(500);
+                    //animationSet.addAnimation(animation);
+                    controller = new LayoutAnimationController(animation,1);
+                    relativeLayout.setLayoutAnimation(controller);
+                    //textViewHint.setVisibility(View.GONE);
+                } else {
+                    createTable();
+                }
+                break;
+        }
+        return true;
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -324,35 +358,4 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         ExitApplication.getInstance().exit();
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.bt_create:
-                tableName = editText.getText().toString();
-                Table table = new Table(tableName);
-                tableList.add(table);
-                if (tableList.isEmpty()) {
-                    textViewHint.setVisibility(View.VISIBLE);
-                } else {
-                    textViewHint.setVisibility(View.GONE);
-                }
-                adapter.notifyDataSetChanged();
-                createTableLayout.setVisibility(View.GONE);
-                editText.setText("");
-                inputMethodManager = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                break;
-            case R.id.bt_cancel:
-                createTableLayout.setVisibility(View.GONE);
-                if (tableList.isEmpty()) {
-                    textViewHint.setVisibility(View.VISIBLE);
-                } else {
-                    textViewHint.setVisibility(View.GONE);
-                }
-                inputMethodManager = (InputMethodManager)v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                break;
-        }
-
-    }
 }
